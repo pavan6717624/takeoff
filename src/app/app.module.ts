c73b91c80883e9e4b4ab0c4af74fec5748c32ae2
pavan6717624 <pvn.kumar4@gmail.com>
2021-07-16 21:38:46 +0530

    'depoly'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
+import { MastheadComponent } from './component/masthead/masthead.component';
+import { AboutComponent } from './component/about/about.component';
+import { ServicesComponent } from './component/services/services.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { CallComponent } from './component/call/call.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    MastheadComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    CallComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
