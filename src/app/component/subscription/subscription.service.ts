725324300a7a7f4f2679cdd6ea065760d0001a06
pavan6717624 <pvn.kumar4@gmail.com>
2021-08-20 16:30:44 +0530

    deploy

--- src/app/component/subscription/subscription.service.ts
+++ src/app/component/subscription/subscription.service.ts
@@ -12,20 +12,20 @@ export class SubscriptionService {
 
   getOrderId(): Observable<any>
   {
-    //return this.http.get( 'http://localhost:8081/getOrderId' );
-    return this.http.get( 'http://localhost:8081/getOrderId' );
+    //return this.http.get( 'https://takeoff-pavan.herokuapp.com/getOrderId' );
+    return this.http.get( 'https://takeoff-pavan.herokuapp.com/getOrderId' );
   }
 
   async checkRefererId(formData: FormData): Promise<any>
   {
-    // return await this.http.post( 'http://localhost:8081/checkRefererId',formData ).toPromise();
-    return await this.http.post( 'http://localhost:8081/checkRefererId',formData ).toPromise();
+    // return await this.http.post( 'https://takeoff-pavan.herokuapp.com/checkRefererId',formData ).toPromise();
+    return await this.http.post( 'https://takeoff-pavan.herokuapp.com/checkRefererId',formData ).toPromise();
   }
   
   getSubscription(subscription:SubscriptionDTO): Observable<any>
   {
-  // return this.http.post('http://localhost:8081/subscribe',subscription );
-    return this.http.post('http://localhost:8081/subscribe',subscription );
+  // return this.http.post('https://takeoff-pavan.herokuapp.com/subscribe',subscription );
+    return this.http.post('https://takeoff-pavan.herokuapp.com/subscribe',subscription );
   }
 
 }
