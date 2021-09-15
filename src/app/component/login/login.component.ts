fc04dac941911a2b174cd0e7ba4e5fbb8bd2cd8b
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2021-09-15 12:48:35 +0530

    depoly

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -21,6 +21,7 @@ export class LoginComponent implements OnInit {
 
 
   userid: string = "";
+  passwordVisible = false;
   password: string = "";
   logginStatus: boolean = false;
   loginStatus: LoginStatus = new LoginStatus();
