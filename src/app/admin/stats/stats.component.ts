a793a5ac17922c6fe08a5a613bffd7db36990843
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2021-10-07 18:14:02 +0530

    depoly

--- src/app/admin/stats/stats.component.ts
+++ src/app/admin/stats/stats.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { AdminService } from '../admin.service';
+
+export class StatsDTO
+{
+  roleName: string = '';
+  todayCount: number = 0;
+  monthCount: number = 0;
+  totalCount: number = 0;
+}
 
 @Component({
   selector: 'app-stats',
@@ -7,9 +17,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsComponent implements OnInit {
 
-  constructor() { }
+  stats : StatsDTO[] = [];
+  loading = false;
+  walletBalance: string = '';
+  constructor(private adminService: AdminService, private msg: NzMessageService) { }
 
   ngOnInit(): void {
+
+    this.getUserStats();
+    this.getWalletBalance();
+  }
+
+  getUserStats()
+  {
+    this.loading=true;
+    this.adminService.getUserStats().subscribe(
+      (res : any) => { console.log(res);   this.stats = res; this.loading=false;},
+      (err) => { console.log(err); this.msg.create('error','Error Occured at Server. Please try again.'); this.loading=false;}
+     
+       );
+  }
+
+  getWalletBalance()
+  {
+    this.adminService.getWalletBalance().subscribe(
+      (res : any) => { console.log(res);   this.walletBalance = res.walletBalance; },
+      (err) => { console.log(err); this.msg.create('error','Error Occured at Server. Please try again.');}
+     
+       );
   }
 
 }
