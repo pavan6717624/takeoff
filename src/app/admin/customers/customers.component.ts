72cc4b18bf73bfbdf8281c89b462268d21248da2
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2021-10-07 16:52:06 +0530

    depoly

--- src/app/admin/customers/customers.component.ts
+++ src/app/admin/customers/customers.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { CustomerAccount } from 'src/app/user/account/account.component';
+import { AdminService } from '../admin.service';
+
 
 @Component({
   selector: 'app-customers',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adminService: AdminService, private msg: NzMessageService) { }
+
+  customerDetails: CustomerAccount[]=[];
 
   ngOnInit(): void {
+    this.getAllCustomerAccountDetails();
+  }
+
+  loading=false;
+
+  getAllCustomerAccountDetails()
+  {
+    this.loading=true;
+    this.adminService.getAllCustomerAccountDetails().subscribe(
+      (res : any) => { console.log(res);   this.customerDetails = res; this.loading=false;},
+      (err) => { console.log(err); this.msg.create('error','Error Occured at Server. Please try again.'); this.loading=false;}
+     
+       );
   }
 
 }
