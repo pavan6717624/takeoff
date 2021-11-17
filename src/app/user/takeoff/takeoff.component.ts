5a828e3f6ce7b56217f04bfb2d53405fb0bd4c76
pavan6717624 <84194405+pavan6717624@users.noreply.github.com>
2021-11-17 16:15:40 +0530

    depoly

--- src/app/user/takeoff/takeoff.component.ts
+++ src/app/user/takeoff/takeoff.component.ts
@@ -730,6 +730,7 @@ downloadCoupon(item:Coupon)
 
   filterCoupons() {
     this.coupons = [];
+    this.noMoreImages=false;
     this.bottom=false;
     this.filterVisible = false;
     this.businessLogic();
